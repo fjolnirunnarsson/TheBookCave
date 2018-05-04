bbe8a0fe2c29c1ff9d30e6a7b62d8d64574e338d
Fjolnir Unnarsson <FjolnirUnnarsson@Fjolnirs-MacBook-Air.local>
2018-05-04 17:03:41 +0000

    laga villu

--- Data/Lists.cs
+++ Data/Lists.cs
@@ -14,9 +14,8 @@ namespace TheBookCave.Data
                      Year = 0, Image = "", Genre = "", Rating = 0, Price = 0, Discount = 0, BoughtCopies = 0, Quantity = 100, AuthorId = 1
                 },
             };
-
-        }
         */
+        }
 
 
     }
